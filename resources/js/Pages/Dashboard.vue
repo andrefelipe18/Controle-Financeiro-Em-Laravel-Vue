78509be447c0f421708fbcc70416f57a9eb8ba52
André Domingues <dominguesstroppa@gmail.com>
2023-02-08 15:33:07 -0300

    Setup concluido

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,12 @@
+<script setup>
+import AppLayout from '@/Layouts/AppLayout.vue';
+import Welcome from '@/Components/Welcome.vue';
+</script>
+
+<template>
+    <AppLayout title="Painel Principal">
+        <q-btn dense flat icon="airplay" class="gt-xs" />
+      <q-btn dense flat icon="battery_charging_full" />
+      <q-btn dense flat icon="wifi" />
+    </AppLayout>
+</template>
---
+++ vite.config.js
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: 'resources/js/app.js',
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+        quasar({
+            autoImportComponentCase: 'kebab',
+            autoImportComponentPrefix: 'Q',
+            rtlSupport: false,
+
+        }),
+    ],
+});
